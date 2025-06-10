4a43a1bc8b0e647b6a84b00154d3aadfb8dbdd67
Stephanie-17 <Stephanie>
2025-06-10 23:17:29 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+window.addEventListener('keydown',(e)=>{
+   let keyHtml = `<div class="key">
+            <p>${e.key===" "?"Space":e.key}</p>
+            <small>event.key</small>
+        </div>
+         <div class="key-code">
+            <p>${e.keyCode}</p>
+            <small>event.keyCode</small>
+        </div>
+        <div class="event-code">
+            <p>${e.code}</p>
+            <small>event.code</small>
+        </div>`
+
+        document.querySelector('.event').innerHTML = keyHtml
+
+})
+
+
+
+
+/*<div class="key">
+            <p></p>
+            <small>event.key</small>
+        </div>
+         <div class="key-code">
+            <p></p>
+            <small>event.keyCode</small>
+        </div>
+        <div class="event-code">
+            <p></p>
+            <small>event.code</small>
+        </div>*/
+        
